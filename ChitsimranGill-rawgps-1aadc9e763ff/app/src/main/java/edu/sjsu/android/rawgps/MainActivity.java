e240b16cfc11e7b49cfa16d740c9fcecea039fa5
ChitsimranGill <36089167+ChitsimranGill@users.noreply.github.com>
2020-10-06 01:17:24 -0700

    Add files via upload

---
+++ ChitsimranGill-rawgps-1aadc9e763ff/app/src/main/java/edu/sjsu/android/rawgps/MainActivity.java
@@ -0,0 +1,36 @@
+package edu.sjsu.android.rawgps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    Button btnShowLocation;
+    GPSTracker gps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                gps = new GPSTracker(MainActivity.this);
+                if(gps.canGetLocation()){
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    Toast.makeText(getApplicationContext(), "Your location is - \nLat: "+ latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else {
+                    gps.showSettingsAlert();
+
+                }
+            }
+        });
+    }
+}
